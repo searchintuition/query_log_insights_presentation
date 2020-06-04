55898fb32fa2d62e47f6245eb3689fc682a9dabe
Peter Dixon-Moses <peter@searchintuition.com>
2020-06-03 23:03:14 -0400

    Added 2_Related_Queries

---
+++ related/transform.py
@@ -0,0 +1,29 @@
+import pandas
+import json
+from slugify import slugify
+
+
+def reshape(df):
+    return df.groupby('document')['query'] \
+        .apply(list) \
+        .reset_index()
+
+
+def preload(df):
+    bulk_ready = []
+    jsonl = df.to_json(orient='records', lines=True, index=True)
+    [add(j, bulk_ready) for j in jsonl.split('\n')]
+
+    return bulk_ready
+
+
+def add(jsonstring, bulk):
+    record = json.loads(jsonstring)
+    leader = {
+      "index": {
+        "_index": "related",
+        "_id": record["document"]
+      }
+    }
+    bulk.append(leader)
+    bulk.append(record)
